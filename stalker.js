f513ac39c455a929dbfec94dc63c3d6c79bcd91a
Tarannum <abs.tarannum007@gmail.com>
2017-10-03 01:11:43 +0200

    source changed to stalker in index.HTML and stalker .js added

---
+++ stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Hello, What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Nice to meet you "+firstName+" "+lastName);
+console.log("You are "+age+" years old.");
