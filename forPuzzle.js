f4930e620c4111261e37a29635d2f21e2891b106
Tarannum <abs.tarannum007@gmail.com>
2017-10-04 10:00:41 +0200

    forPuzzle.js and linked in index.html

--- forPuzzle.js
+++ forPuzzle.js
@@ -1,2 +1,27 @@
 alert("For Loop Puzzle");
 var i = 0;
+
+//first
+console.log("Problem 1");
+for (i = -10; i<20; i++) {
+  console.log(i);
+}
+//second
+console.log("second");
+for (i = 10; i<=40; i+=2) {
+  console.log(i);
+}
+//third
+console.log("third");
+for (i = 300; i<=333; i++) {
+if (i%2!=0) {
+  console.log(i);
+  }
+}
+//fourth
+console.log("fourth");
+for (i = 5; i<=50; i++) {
+  if ((i%3==0)&&(i%5==0)) {
+    console.log(i);
+  }
+}
