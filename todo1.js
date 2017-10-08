0494dde8fecc66357ac521be111803d81daa41d7
Tarannum <abs.tarannum007@gmail.com>
2017-10-08 17:00:29 +0200

    New todo.html with a new todo.js file for array excercise

---
+++ todo1.js
@@ -0,0 +1,15 @@
+alert("Welcome to the Todo App");
+
+var input = prompt("Please Enter a command :");
+var toDo = [];
+
+while (input!="quit") {
+  if (input=="new") {
+    var element = prompt("Enter a task");
+    toDo.push(element);
+  }
+  else if (input=="list") {
+
+  }
+
+}
